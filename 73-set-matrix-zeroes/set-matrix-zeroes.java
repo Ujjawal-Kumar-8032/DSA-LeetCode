9c770584a185815cd051f73c9f4b70df969ab9b0
UJJAWAL KUMAR <ujjawal8032kumar@outlook.com>
2026-01-29 10:42:56 +0530

    Time: 1 ms (59.28%) | Memory: 47.3 MB (81.38%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -2,21 +2,43 @@ class Solution {
     public void setZeroes(int[][] nums) {
         int m = nums.length;
         int n = nums[0].length;
-        HashSet<Integer> set1 = new HashSet();
-        HashSet<Integer> set2 = new HashSet();
+        boolean col=false;
+        boolean row=false;
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+            if(nums[i][0]==0){
+                col=true;
+                break;
+            }
+        }
+        for(int j=0;j<n;j++){
+            if(nums[0][j]==0){
+                row=true;
+                break;
+            }
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
                 if(nums[i][j]==0){
-                    set1.add(i);
-                    set2.add(j);
+                    nums[i][0]=0;
+                    nums[0][j]=0;
                 }
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(set1.contains(i) || set2.contains(j)){
-                    nums[i][j] = 0;
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(nums[i][0]==0 || nums[0][j]==0){
+                    nums[i][j]=0;
                 }
+            }
+        }
+        if(row){
+            for(int i=0;i<n;i++){
+                nums[0][i]=0;
+            }
+        }
+        if(col){
+            for(int i=0;i<m;i++){
+                nums[i][0]=0;
             }
         }
     }
