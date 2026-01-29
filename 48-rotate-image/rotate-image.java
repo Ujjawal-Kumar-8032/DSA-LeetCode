d48e8a2851d8287e3469630d0f2d984f605c2d7a
UJJAWAL KUMAR <ujjawal8032kumar@outlook.com>
2026-01-29 11:34:44 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (63.63%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void rotate(int[][] nums) {
+        int n=nums.length;
+        // Transpose then swap
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int temp = nums[i][j];
+                nums[i][j]=nums[j][i];
+                nums[j][i]=temp;
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+                int temp = nums[i][j];
+                nums[i][j]=nums[i][n-1-j];
+                nums[i][n-1-j]=temp;
+            }
+        }
+    }
+}
