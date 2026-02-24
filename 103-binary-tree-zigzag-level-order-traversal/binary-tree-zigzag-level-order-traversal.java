e5e75528aa92bd44874f76608a6ec25ceb4878b8
UJJAWAL KUMAR <ujjawal8032kumar@outlook.com>
2026-02-24 14:40:43 +0530

    Time: 1 ms (80.53%) | Memory: 43.7 MB (62.56%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -1,34 +1,25 @@
 class Solution {
-    class Pair{
-        int level;
-        TreeNode TreeNode;
-        public Pair(TreeNode TreeNode, int level){
-            this.TreeNode=TreeNode;
-            this.level=level;
-        }
-    }
-    public void levelTraverse(TreeNode root,List<List<Integer>> ans){
-        Queue<Pair> q = new LinkedList<>();
-        if(root!=null) q.add(new Pair(root,0));
-        while(q.size()>0){
-            Pair front = q.remove();
-            int lvl = front.level;
-            TreeNode node = front.TreeNode;
-            if(lvl==ans.size()) ans.add(new ArrayList<>());
-            if(lvl%2==0){
-                ans.get(lvl).add(node.val);
-            }else{
-                ans.get(lvl).add(0,node.val);
-            }
-            if(node.left!=null) q.add(new Pair(node.left,lvl+1));
-            if(node.right!=null) q.add(new Pair(node.right,lvl+1));
-
-        }
-
-    }
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
-        levelTraverse(root,ans);
+        Queue<TreeNode> q = new LinkedList<>();
+        if(root!=null) q.add(root);
+        int level=0;
+        while(q.size()>0){
+            int size = q.size();
+            List<Integer> list = new ArrayList<>();
+            for(int i=0;i<size;i++){
+                TreeNode node = q.remove();
+                list.add(node.val);
+                if(node.left!=null) q.add(node.left);
+                if(node.right!=null) q.add(node.right);
+            }
+            if(level%2==0) ans.add(list);
+            else{
+               Collections.reverse(list);
+               ans.add(list);
+            }
+            level++;
+        }
         return ans;
     }
 }
